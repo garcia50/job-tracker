f73548ad17e3f0f2f4c60b2e2d36cf13b14f5a11
Luis Garcia <garcia@yahoo.com>
2018-02-03 21:39:02 -0700

    Complete jobs crud functionality

---
+++ db/migrate/20180204022501_add_column_categories_to_jobs.rb
@@ -0,0 +1,5 @@
+class AddColumnCategoriesToJobs < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :jobs, :category, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,14 @@ Job.destroy_all
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+CATEGORIES = ["Web Development", "Education", "Finance", "Engineering", "Management", "Accounting", "HR", "Maintenance", "Administration", "Marketing"]
 
-COMPANIES.each do |name|
+COMPANIES.each_with_index do |name, i|
   company = Company.create!(name: name)
+  category = Category.create!(title: CATEGORIES[i])
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample)
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category: category)
     puts "  Created #{company.jobs[num].title}"
   end
 end
